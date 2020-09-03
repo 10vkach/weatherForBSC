9b96ab98d83a106ef76fb59a2b7855d51b121bfb
Dima Zareckiy <>
2020-09-03 11:11:22 +0400

    Небольшая чистка кода

--- weatherForBSC/weatherForBSC/Model/WeatherList.swift
+++ weatherForBSC/weatherForBSC/Model/WeatherList.swift
@@ -9,11 +9,5 @@ class WeatherList {
         citys.forEach({ self.places.append(Weather(city: $0)) })
         tempratureUnits = units
     }
-    
-    func updateAllWeather() {
-        places.forEach({
-            weatherNetworker.currentWeather(inCity: $0.city)
-        })
-    }
 }
 
